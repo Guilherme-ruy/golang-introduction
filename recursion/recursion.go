85a4310dee042f113f17e7023a99c1c6653c4937
Guilherme Ruy <guilherme.ruy@outlook.com>
2026-02-23 21:44:05 -0300

    feat: add closure and recursion examples

---
+++ recursion/recursion.go
@@ -0,0 +1,18 @@
+// Recursão: Capacidade de uma função chamar ela mesma
+// Ex: Calcular fatorial, fibonacci
+
+package main
+
+import "fmt"
+
+func fatorial(x int) int {
+	if x == 0 {
+		return 1
+
+	}
+	return x * fatorial(x-1)
+}
+
+func main() {
+	fmt.Println(fatorial(9))
+}
